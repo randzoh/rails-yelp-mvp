8415f45d30cc3e97cba6b7ad019cb8a224c80d80
Randja Raharijaona <randzoh@outlook.com>
2022-04-12 20:58:08 -0400

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: %i[index show new create]
-
-  resources :restaurants do
+  resources :restaurants, only: %i[index show new create] do
     resources :reviews, only: %i[new create]
   end
 end
